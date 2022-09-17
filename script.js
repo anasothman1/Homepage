fdc14e2c38439ed4395928af83fd1627a7331603
anasothman1 <113244900+anasothman1@users.noreply.github.com>
2022-09-17 13:58:33 +0100

    Update script.js
    Adding links

--- script.js
+++ script.js
@@ -88,3 +88,29 @@ document.getElementById('day').addEventListener('click', (event) => {
 let element_day = document.getElementById('day');
 element_day.style.color = '#330000';
 element_day.style.backgroundColor = '#ffcccc';
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'http://www.codeyourfuture.io ');
+new_a.innerText = 'Code Your Future';
+
+new_li.appendChild(new_a);
+let new_li2 = document.createElement('li');
+let new_a2 = document.createElement('a');
+new_a2.setAttribute("href", 'https://www.google.com/');
+new_a2.innerText = 'Google';
+
+new_li2.appendChild(new_a2);
+let new_li3 = document.createElement('li');
+let new_a3 = document.createElement('a');
+new_a3.setAttribute("href", 'https://www.bbc.co.uk/news');
+new_a3.innerText = 'Bbc News';
+
+new_li3.appendChild(new_a3);
+
+new_li2.appendChild(new_li3);
+
+new_li.appendChild(new_li2);
+
+element_list.appendChild(new_li);
