8cb290cf74b096a7fbe127482807c7672e7858bf
anasothman1 <113244900+anasothman1@users.noreply.github.com>
2022-09-17 14:27:11 +0100

    Update script.js
    Comments

--- script.js
+++ script.js
@@ -134,3 +134,23 @@ document.getElementById('button').addEventListener('click', (event) => {
   element_list.appendChild(new_input);
 
 });
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = getNumberOrString(document.getElementById('text').value);
+
+  element_list.appendChild(new_li);
+
+});
