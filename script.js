fa07bbd7ece5c6b7aface918a9d1623856d0de68
anasothman1 <113244900+anasothman1@users.noreply.github.com>
2022-09-24 14:53:21 +0100

    Update script.js
    Adding list of fruits

--- script.js
+++ script.js
@@ -166,13 +166,3 @@ while (!!fruits.length) {
 }
 
 
-
-document.getElementById('button').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  let new_li = document.createElement('li');
-  new_li.innerText = getNumberOrString(document.getElementById('text').value);
-
-  element_list.appendChild(new_li);
-
-});
-
